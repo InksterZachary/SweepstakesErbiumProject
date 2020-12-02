ea18c71f658245561ac97bccd14647e11b1d567f
Zachary Inkster <z.j.inkster@gmail.com>
2020-12-02 14:53:45 -0600

    Update to previous commit

--- SweepstakesProject/UserInterface.cs
+++ SweepstakesProject/UserInterface.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-    class UserInterface
+    public static class UserInterface
     {
+        public static string GetFirst()
+        {
+            Console.WriteLine("Please enter your first name");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        public static string GetLast()
+        {
+            Console.WriteLine("Please enter your last name");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        public static string GetEmail()
+        {
+            Console.WriteLine("Please enter your email");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        public static double AssignRegistrationNumber() //I want to make this better later: To go through a list of Contestants and assign a number not already given to the new Contestant
+        {
+            Random num = new Random();
+            double regNumber = num.Next(1, 1000);
+            Console.WriteLine("Your registration number is {0}.", regNumber);
+            return regNumber;
+        }
     }
 }
