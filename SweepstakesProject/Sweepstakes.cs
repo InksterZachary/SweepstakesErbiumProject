b6dfbafaabbbbd28fb8c7982ed81aa82777911c3
Zachary Inkster <z.j.inkster@gmail.com>
2020-12-02 19:08:14 -0600

    Filled in method for PrintContestantInfo() and PickWinner()

--- SweepstakesProject/Sweepstakes.cs
+++ SweepstakesProject/Sweepstakes.cs
@@ -9,13 +9,20 @@ namespace SweepstakesProject
     class Sweepstakes
     {
         //member variables
-        Dictionary<double, Contestant> contestants;
+        public Dictionary<double, Contestant> contestants;
         //constructor
 
         //member methods
-        public string Sweepstakes(string name)
-        {
-
+        public Contestant Sweepstakes(string name) //I'm not sure what this one is supposed to do. Is this just supposed to be the name of the actual Sweepstakes or retrieve a contestant from the Dictionary?
+        {                                          //Either change the name so not a constructor OR delete scope and add name of Sweepstakes
+            foreach(KeyValuePair<double,Contestant> person in contestants)
+            {
+                if(person.Value.lastName == name)
+                {
+                    return person.Value;
+                }
+            }
+            return null;
         }
         public void RegisterContestant(Contestant contestant)
         {
@@ -23,11 +30,29 @@ namespace SweepstakesProject
         }
         public Contestant PickWinner()
         {
-
+            Random rand = new Random();
+            foreach (KeyValuePair<double, Contestant> person in contestants)
+            {
+                double regNumber = rand.Next(1, 1000);
+                while (person.Key != regNumber)
+                {
+                    if (person.Key == regNumber)
+                    {
+                        return person.Value;
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(250); //Allows new seed for regNumber
+                        regNumber = rand.Next(1, 1000);
+                    }
+                }
+            }
+            return null;
         }
-        public void PrintContestantInfo()
+        public void PrintContestantInfo(Contestant contestant)
         {
-
+            Console.WriteLine("{0} {1} \n" +
+                "{2}",contestant.firstName,contestant.lastName,contestant.email);
         }
     }
 }
