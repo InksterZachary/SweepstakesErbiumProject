602d0afdc73977d979013b2c97dcbf497dbc31f1
Zachary Inkster <z.j.inkster@gmail.com>
2020-12-03 10:09:03 -0600

    Corrected use of constructor in Sweepstakes

--- SweepstakesProject/Sweepstakes.cs
+++ SweepstakesProject/Sweepstakes.cs
@@ -10,25 +10,29 @@ namespace SweepstakesProject
     {
         //member variables
         public Dictionary<double, Contestant> contestants;
+        public string name;
         //constructor
+        public Sweepstakes(string name)            //I'm not sure what this one is supposed to do. Is this just supposed to be the name of the actual Sweepstakes or retrieve a contestant from the Dictionary?
+        {                                          //Either change the name so not a constructor OR delete scope and add name of Sweepstakes
+                                                   //I deleted return type 'Contestant' and instantiated a new Dictionary because every sweepstakes will have a name and a Dictionary
+            contestants = new Dictionary<double, Contestant>();
+            this.name = name;
+            //foreach(KeyValuePair<double,Contestant> person in contestants)
+            //{
+            //    if(person.Value.lastName == name)
+            //    {
+            //        return person.Value;
+            //    }
+            //}
+            //return null;
+        }
 
         //member methods
-        public Contestant Sweepstakes(string name) //I'm not sure what this one is supposed to do. Is this just supposed to be the name of the actual Sweepstakes or retrieve a contestant from the Dictionary?
-        {                                          //Either change the name so not a constructor OR delete scope and add name of Sweepstakes
-            foreach(KeyValuePair<double,Contestant> person in contestants)
-            {
-                if(person.Value.lastName == name)
-                {
-                    return person.Value;
-                }
-            }
-            return null;
-        }
         public void RegisterContestant(Contestant contestant)
         {
             contestants.Add(contestant.registrationNumber,contestant);
         }
-        public Contestant PickWinner()
+        public Contestant PickWinner() //Maybe to make more efficient: Create foreach loop to create list of all available Keys and then pick random number from key list
         {
             Random rand = new Random();
             foreach (KeyValuePair<double, Contestant> person in contestants)
---
+++ SweepstakesProject/SweepstakesQueueManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+    class SweepstakesQueueManager : ISweepstakesManager
+    {
+        public void InsertSweepStakes()
+        {
+
+        }
+    }
+}
